55229e3cfa7c39c3e52d3927b55ff4dfaa8e6679
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-03-25 16:51:46 -0400

    build it

--- dist/index.js
+++ dist/index.js
@@ -58003,7 +58003,8 @@ async function GetProjectDetails() {
     const excludedProjects = ['GameAssembly', 'UnityFramework', 'Pods'];
     for (const file of files) {
         if (file.endsWith('.xcodeproj')) {
-            if (excludedProjects.some(p => file.includes(`/${p}.xcodeproj`))) {
+            const projectBaseName = path.basename(file, '.xcodeproj');
+            if (excludedProjects.includes(projectBaseName)) {
                 continue;
             }
             core.debug(`Found Xcode project: ${file}`);
